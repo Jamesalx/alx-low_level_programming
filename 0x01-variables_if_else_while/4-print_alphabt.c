ed91d026cc5da397b7c49839e66dc082b7f7ca6e
James Otieno <bill.ochola@gmail.com>
2023-02-17 14:13:00 -0800

    Task 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -14,15 +14,15 @@ int main(void)
 {
 	char low, e, q;
 
-	e = 'e'
-	q = 'q'
+	e = 'e';
+	q = 'q';
 
 	for (low = 'a'; low = 'z'; low++)
 	{
 	if (low != e && low != q)
 	putchar(low);
 	}
-	putchar('\n')
+	putchar('\n');
 	return (0);
 }
 
