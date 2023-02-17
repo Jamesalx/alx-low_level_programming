e648eeacdb2d4a247e943798f4103eb3756acfc4
James Otieno <bill.ochola@gmail.com>
2023-02-17 11:43:14 -0800

    Task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,12 +5,20 @@
 int main(void)
 {
 	int n;
-	if 
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	printf("%d is a positive number,n\n");
-	printf("%d is a negative number,n\n");
-	printf("%d\n", rand_num);
-	return (0);
+	if( n > o )
+	{	
+	printf( "%d is positive ,n\n" );
+	}
+	else if( n < 0 )
+	{
+	printf( "%d is negative ,n\n" );
+	}
+	else( n == 0 ) 
+	{
+	printf( "%d is zero,n\n" );
+	}
+	return(0);
 }
