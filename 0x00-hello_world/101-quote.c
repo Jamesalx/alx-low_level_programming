d9e88dc3cf99efab31ad6b07ece9a39546228b5e
James Otieno <bill.ochola@gmail.com>
2023-02-18 00:46:58 -0800

    Task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main-Write a C program that prints something 
+ *
+ * Return: (1) success
+ */
+
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}
+
