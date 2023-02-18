0bfb41b8cd31439c08d1aaae892e24f12afc83df
James Otieno <bill.ochola@gmail.com>
2023-02-17 23:01:01 -0800

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,13 @@
-#!/bin/bash
 #include <stdio.h>
 
-int main(void){
+/**main-C program that prints exactly  followed by a new line
+ *
+ *Return (0) Success
+*/
+
+int main(void)
+{	
 	printf("with proper grammar, but the outcome is a piece of art,\n");
+	
 	return (0);
 }
