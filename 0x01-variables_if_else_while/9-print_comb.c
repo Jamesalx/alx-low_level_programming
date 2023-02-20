acbd3afe6d9eeb80b33b5d576fd05213d5804b71
James Otieno <bill.ochola@gmail.com>
2023-02-20 10:45:47 -0800

    Patience, persistence and perspiration

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -3,25 +3,21 @@
 #include <time.h>
 
 /**
- * main-a program that prints the lowercase alphabet in reverse
+ * main-a program that prints all possible combinations of single-digit numbers
  *
  * Return: 0 Always (success)
  */
 
 int main(void)
-
+{
 	int d;
 
-	for (d = '0'; d <= '9'; d++)
-{
-	putchar(d);
-	if (d != '9')
+	for (d = 48; d <= 57; d++)
 	{
+	putchar(d);
 	putchar(',');
 	putchar(' ');
 	}
-	}
 	putchar('\n');
-	return(0);
-	
+	return (0);
 }
