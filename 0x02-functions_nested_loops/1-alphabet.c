c98965e72837da4ccfc56546d27e9f818ce1c51c
James Otieno <bill.ochola@gmail.com>
2023-02-21 12:54:31 -0800

    1. I sometimes suffer from insomnia

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,20 +1,17 @@
-#include <unistd.h>
+#include "main.h"
 
 /**
  * main-function that prints the alphabet, in lowercase.
  *
- * Return: (0) Success
+ *
  */
 
 void print_alphabet(void);
-
-int main(void);
 {
 	(ch = 'a');
 
 	for (ch = 'a'; <= 'z'; ch++);
-	putchar(ch);
-	putchar('\n');
 
-	return (0);
+	_putchar(ch);
+	_putchar('\n');
 }
