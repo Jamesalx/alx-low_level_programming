9770d3c559d21f7ed8ef9ffabefbb4d49360d958
James Otieno <bill.ochola@gmail.com>
2023-02-21 04:28:16 -0800

    0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+
+/**
+ * main-Write a program that prints _putchar, followed by a new line.
+ *
+ *
+ *Return: (0) success
+ *
+ */
+
+int main(void)
+{
+	char ch = '_putchar';
+
+	putchar(ch, '\n');
+
+	return (0);
+}
