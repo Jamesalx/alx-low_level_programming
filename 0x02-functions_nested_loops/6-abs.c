1ea8c46707e833ce87bca1e698e581fc1ff60b58
James Otieno <bill.ochola@gmail.com>
2023-02-22 01:21:17 -0800

    absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main- function that computes the absolute value of an integer.
+ *
+ *  Return: Always 0.
+ */
+
+int _abs(int)
+{
+	if (i >= 0)
+	{
+	return (i);
+	}
+	else
+	{
+		return (i * -1);
+	}
+	return (0);
+}
+
+
