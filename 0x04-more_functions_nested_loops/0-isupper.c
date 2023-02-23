803505bd230c0b5cd6e3614065edb7218d947066
James Otieno <bill.ochola@gmail.com>
2023-02-23 04:33:00 -0800

    Task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - a function that checks for uppercase character
+ *Returns 1 if c is uppercase
+ *Returns 0 otherwise
+ *
+ */
+
+int _isupper(int c)
+{
+	int ch = 'c';
+
+	if (isupper(ch))
+	{
+	_putchar("1");
+	}
+	else
+	{
+	_putchar("0");
+	}
+	return (0);
+}
