d6c93bef50cc6091a88c50c60264ff25f6ce653e
James Otieno <bill.ochola@gmail.com>
2023-02-24 05:30:56 -0800

    Task 0

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+void positive_or_negative(int i)
+{
+	if (i > 0)
+		printf("%d is a positive number\n", i);
+
+	else if (1 < 0)
+		printf("%d is a negative number,\n", i);
+	
+	else
+		printf("%d is zero \n", i);
+}
+
+
