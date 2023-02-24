e4141c36adea3f9c0f24a9ab59cf8a16f53abe8f
James Otieno <bill.ochola@gmail.com>
2023-02-23 23:41:47 -0800

    Task 7

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - function that draws a straight line in the terminal.
+ *
+ * Return: void
+ */
+
+void print_line(int n)
+{
+	int i;
+	int n;
+
+	for (i = 0; i < n; i++)
+	{
+	_putchar('_');
+	}
+	_putchar('\n');
+}
