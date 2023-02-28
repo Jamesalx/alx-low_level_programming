1fda074345925f66d5a06080ecf9880660f8512f
James Otieno <bill.ochola@gmail.com>
2023-02-27 23:51:25 -0800

    4. I can only go one way

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+#include <string.h>
 
 /**
  * main - function that prints a string, in reverse
@@ -10,4 +11,15 @@
 
 void print_rev(char *s)
 {
-	
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	for (i -= 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
