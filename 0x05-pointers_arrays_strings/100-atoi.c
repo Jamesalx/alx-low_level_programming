0771ac3dda52946df8471c288d66d0ceff0a53e8
James Otieno <bill.ochola@gmail.com>
2023-03-01 23:26:58 -0800

    10. Great leaders

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -16,6 +16,8 @@ int _atoi(char *s)
 
 	while (s[c])
 	{
+	if (s[c] == 45)
+	{
 		min *= -1;
 	}
 	while (s[c] >= 48 && s[c] <= 57)
@@ -29,8 +31,7 @@ int _atoi(char *s)
 	break;
 	}
 	c++;
-	{
-	ni *= min;
 	}
+	ni *= min;
 	return (ni);
 }
