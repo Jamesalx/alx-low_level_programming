4e8b7a3a46ce598c86d2c1546115535a05a83c1a
James Otieno <bill.ochola@gmail.com>
2023-03-01 22:20:25 -0800

    7. Winning is only half of it

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,9 +1,8 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * main - function that prints half of a string, followed by a new line. 
- *
+ * puts_half - prints half of a string followed by a new line.
+ *@str:the string to print
  * Return:void.
  */
 
@@ -12,7 +11,22 @@ void puts_half(char *str)
 	int i = 0;
 	int j;
 
-	while(str[j] !=
-	str = 
-
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	if (i % 2 == 1)
+	{
+		j = (i - 1) / 2;
+		j += 1;
+	}
+	else
+	{
+		j = i / 2;
+	}
+	for (; j < i; j++)
+	{
+		_putchar(str[j]);
+	}
+	_putchar('\n');
 }
