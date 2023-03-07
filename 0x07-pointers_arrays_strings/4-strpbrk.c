85db452a931940c672d3ea7b33d0064e4aa6a839
James Otieno <bill.ochola@gmail.com>
2023-03-06 23:30:30 -0800

    4. strpbrk

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -3,7 +3,7 @@
 #include <string.h>
 
 /**
- * _strpbrk - function locates the first occurrence in the string 's' 
+ * _strpbrk - function locates the first occurrence in the string 's'
  * of any of the bytes in the string 'accept'
  *@s: the string to be scanned
  *@accept:string containing the characters to match
@@ -12,5 +12,15 @@
 
 char *_strpbrk(char *s, char *accept)
 {
+	int a, b;
 
+	for (a = 0; s[a] != '\0'; a++)
+	{
+		for (b = 0; accept[b] != 0; b++)
+		{
+			if (s[a] == accept[b])
+			return (s + a);
+		}
+	}
+return ('\0');
 }
