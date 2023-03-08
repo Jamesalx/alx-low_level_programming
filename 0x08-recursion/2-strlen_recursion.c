ea99b732ec808fd328fb3f0e90d58028af0576a7
James Otieno <bill.ochola@gmail.com>
2023-03-08 06:40:31 -0800

    2. Dreams feel real

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strlen_recursion - function that returns the length of a string
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int n;
+
+	n = _strlen_recursion("Corbin Coleman");
+	printf("%d\n", n);
+	return (0);
+}
+int _strlen_recursion(char *s)
+{
+	char *s;
+	int j;
+
+	int len = _putchar("%s\n", *s);
+	_putchar("%d\n", j - 1);
+	return(0);
+}
