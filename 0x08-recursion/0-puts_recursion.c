397b6a96cd5dbddd5e7242d40e801c6322a6193d
James Otieno <bill.ochola@gmail.com>
2023-03-08 02:23:32 -0800

    0. She locked away a secret

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - function that prints a string,
+ * followed by a new line.
+ *@s: string input
+ * Return: Always 0.
+ */
+void _puts_recursion(char *s)
+{
+	char i;
+
+	_putchar("%s\n", i);
+}
