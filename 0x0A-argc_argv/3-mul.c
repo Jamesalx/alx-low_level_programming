47b13d7f7382881bd95d5e9d0ccaf5ef889789c1
James Otieno <bill.ochola@gmail.com>
2023-03-11 07:58:09 -0800

    Task3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -8,14 +8,14 @@
  * Return: Always (0)
  */
 
-int main(int argc, char argv[])
+int main(int argc, char *argv[])
 {
 	int num1 = 0, num2 = 0;
 
 	if (argc == 3)
 	{
-		num1 = atoi(argv[1]);
-		num2 = atoi(argv[2]);
+		num1 = _atoi(argv[1]);
+		num2 = _atoi(argv[2]);
 		printf("%d\n", num1 * num2);
 	}
 	else
