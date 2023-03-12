87656953350768fa7d06975125b28f7e0fbd4fd5
James Otieno <bill.ochola@gmail.com>
2023-03-11 23:11:44 -0800

    Task4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,42 +1,61 @@
 #include "main.h"
-#include<stdio.h>
+#include<stdlib.h>
 #include <string.h>
+#include <ctype.h>
+#include <stdio.h>
 
 /**
- *main - a program that adds positive numbers
- *@argc:count of the arguments
- *@argv:an array of pointers to the strings
- *which are those arguments
- *Return: (1) success
- */
+* check_num - check - string there are digit
+* @str: array str
+*
+* Return: Always 0 (Success)
+*/
+	int check_num(char *str)
+{
+	unsigned int count;
 
+	count = 0;
+	while (count < strlen(str))
+	{
+		if (!isdigit(str[count]))
+		{
+			return (0);
+		}
+		count++;
+	}
+	return (1);
+}
+/**
+* main - Print the name of the program
+* @argc: Count arguments
+* @argv: Arguments
+*
+* Return: Always 0 (Success)
+*/
 int main(int argc, char *argv[])
 {
-	int i;
-	unsigned int k, sum = 0;
-	char *e;
+	int count;
+	int str_to_int;
+	int sum = 0;
 
-	if (argc > 1)
+	count = 1;
+	while (count < argc)
 	{
-		for (i = 1; i < argc; i++)
-			e = argv[i];
+		if (check_num(argv[count]))
+
 		{
-			for (k = 0; k < strlen(e); k++)
-			{
-				if (e[k] < 48 || e[k] > 57)
-				{
-					printf("error\n");
-					return (1);
-				}
-			}
-			sum += atoi(e);
-			e++;
+			str_to_int = atoi(argv[count]);/*ATOI --> convert string to int*/
+			sum += str_to_int;
+		}
+
+			/*Condition if one of the number contains symbols that are not digits*/
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+		count++;
 		}
 		printf("%d\n", sum);
-	}
-	else
-	{
-		printf("0\n");
-	}
-	return (0);
+		return (0);
 }
