7cc6ea9b932b8ee5537013411219872b23915142
James Otieno <bill.ochola@gmail.com>
2023-03-21 12:59:52 -0700

    3. Function-like macro

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,14 @@
+#ifndef SUM(x, y)
+#define SUM(x, y) (x + y)
+/**
+ *main - computes the sum of the numbers x and y
+ *
+ *Return: (0);
+ */
+int main(void)
+{
+	int num1, num2;
+	printf("sum(%d, %d) = %d\n", x, y, SUM(x, y));
+	return (0);
+}
+#endif
