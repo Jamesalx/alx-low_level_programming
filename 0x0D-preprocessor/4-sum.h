4bd76ab71f34f4ad2a6806c7ef717db4d5de5363
James Otieno <bill.ochola@gmail.com>
2023-03-21 13:04:07 -0700

    4. SUM

--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -1,14 +1,6 @@
-#ifndef SUM(x, y)
-#define SUM(x, y) (x + y)
-/**
- *main - computes the sum of the numbers x and y
- *
- *Return: (0);
- */
-int main(void)
-{
-	int num1, num2;
-	printf("sum(%d, %d) = %d\n", x, y, SUM(x, y));
-	return (0);
-}
+#ifndef SUM_H
+#define SUM_H
+
+#define SUM(x, y) ((x) + (y))
+
 #endif
