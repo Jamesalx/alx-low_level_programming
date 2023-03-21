de1659da92668dc43ab8978e2c00cbe40d0eb373
James Otieno <bill.ochola@gmail.com>
2023-03-21 07:50:58 -0700

    0. Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,28 @@
+#ifndef "DOG.H"
+#define "DOG.H"
+/**
+ *main - Defintion of struct dog
+ *struct dog: structure with elements
+ *return: (0);
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+/**
+ *main - Defintion of struct dog
+ *struct dog: structure with elements
+ *return: (0);
+ */
+int main(void)
+{
+	struct dog the_dog;
+
+	user.name = "foo *bar"
+	user.age = "2.3"
+	user.name = "owner"
+	return (0);
+}
+#endif
