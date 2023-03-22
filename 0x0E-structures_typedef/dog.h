be74f12411c77eb1b8e24df6f6b4cd745fb0c979
James Otieno <bill.ochola@gmail.com>
2023-03-22 12:16:08 -0700

    Task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,15 @@
-#ifndef "DOG.H"
-#define "DOG.H"
+#ifndef "DOG_H"
+#define "DOG_H"
+
 /**
- *main - Defintion of struct dog
- *struct dog: structure with elements
- *return: (0);
- */
+* struct dog - a dog's basic info
+* @name: First member
+* @age: Second member
+* @owner: Third member
+*
+* Description: Longer description
+*/
+
 struct dog
 {
 	char *name;
@@ -12,17 +17,14 @@ struct dog
 	char *owner;
 };
 /**
- *main - Defintion of struct dog
- *struct dog: structure with elements
- *return: (0);
- */
-int main(void)
-{
-	struct dog the_dog;
+* dog_t - typedef for struct dog
+*/
+	typedef struct dog dog_t;
 
-	user.name = "foo *bar"
-	user.age = "2.3"
-	user.name = "owner"
-	return (0);
-}
+	void init_dog(struct dog *d, char *name, float age, char *owner);
+	void print_dog(struct dog *d);
+	dog_t *new_dog(char *name, float age, char *owner);
+	void free_dog(dog_t *d);
+	char *_strcpy(char *dest, char *src);
+	int _strlen(char *s);
 #endif
