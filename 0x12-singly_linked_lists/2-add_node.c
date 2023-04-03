0715a8b921ebfa0d1b10ca3d2be516e1854c7675
James Webdev <bill.ochola@gmail.com>
2023-04-03 19:25:57 +0300

    Task 2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -3,18 +3,38 @@
 #include <stdio.h>
 #include "lists.h"
 /**
- * struct list_s - singly linked list
+ * struct list_t - singly linked list
  * @str: string - (malloc'ed string)
- * @len: length of the string
  * @next: points to the next node
- *
- * Return:
+ * @nh_node: new header node
+ * @n_str: new string on header node
+ * Return: new head node
  */
-typedef struct list_s
+typedef struct list_t
 {
     char *str;
     unsigned int len;
     struct list_s *next;
 } list_t;
 list_t *add_node(list_t **head, const char *str)
+{
+	list_t *nh_node;
+	char *n_str;
+	
+	nh_node = malloc(sizeof(list_t));
+	if(nh_node == NULL)
+	{
+		return (NULL);
+	}
+	n_str = strdup(str);
+	if (n_str == NULL)
+	{
+		free(nh_node);
+		return (NULL);
+	}
+	nh_node -> str = n_str;
+	nh_node -> next = *head;
+	*head = nh_node;
 
+	return *head;
+}
